44877a7ef8c79c5e998a234a7cbf3d80afd5187b
Aaron Popp <a_popp@hotmail.com>
2017-03-26 20:42:35 -0500

    Finished TechJobsMVC, still may want to clean up table styling

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,46 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        //takes in two parameters, 'type of search' and 'search term'
+
+        // [Route("/Search/Index")]
+        // [HttpPost]
+        // public IActionResult Search(string searchType, string searchTerm)
+        // {
+            /* Fetch results
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindAll();
+                ViewBag.jobs = jobs;
+                ViewBag.columns = ListController.columnChoices;
+                return View();
+            }
+            else
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = jobs;
+                ViewBag.columns = ListController.columnChoices;
+                return View();
+            }*/
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.jobs = jobs;
+            }
+
+            else
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = jobs;
+            }
+
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search By ";
+            return View("Index");
+        }
 
     }
 }
